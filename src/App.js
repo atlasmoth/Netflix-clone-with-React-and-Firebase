f9e460d2f3e9ce40e15b7a04d332985fd4fc41b3
atlasmoth <ebuka422@gmail.com>
2020-09-25 19:37:56 +0100

    added rest of files

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ function App() {
     <Jumbotron.Container>
       {jumboData.map((data) => (
         <Jumbotron key={data.id} direction={data.direction}>
-          <p>{data.title}</p>
-          <p>{data.subTitle}</p>
-          <p>{data.image}</p>
-          <p>{data.alt}</p>
+          <Jumbotron.Panel>
+            <Jumbotron.Title>{data.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{data.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Panel>
+          <Jumbotron.Panel>
+            <Jumbotron.Image alt={data.alt} src={data.image} />
+          </Jumbotron.Panel>
         </Jumbotron>
       ))}
     </Jumbotron.Container>
--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -6,10 +6,21 @@ export default function Jumbotron({
   direction = "row",
   ...restProps
 }) {
-  console.log(direction);
   return <Inner direction={direction}>{children}</Inner>;
 }
 
 Jumbotron.Container = function ({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+};
+Jumbotron.Panel = function ({ children, ...restProps }) {
+  return <Panel {...restProps}>{children}</Panel>;
+};
+Jumbotron.Image = function ({ children, ...restProps }) {
+  return <Image {...restProps} />;
+};
+Jumbotron.Title = function ({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+Jumbotron.SubTitle = function ({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
 };
