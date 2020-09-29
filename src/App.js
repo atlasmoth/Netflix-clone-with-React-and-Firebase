1134a56dead615f97c10acde5b407d44cc091036
atlasmoth <ebuka422@gmail.com>
2020-09-29 22:31:46 +0100

    done for today

--- src/App.js
+++ src/App.js
@@ -3,18 +3,40 @@ import React from "react";
 import * as Routes from "./constants/routes";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
 import Browse from "./pages/Browse";
 import Signin from "./pages/Signin";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import SignUp from "./pages/Signup";
 
 // import { firebase } from "./lib/firebase.prod";
 
 function App() {
+  const user = null;
   return (
     <Switch>
-      <Route exact path={Routes.SIGN_IN} component={Signin} />
-      <Route exact path={Routes.SIGN_UP} component={Signup} />
-      <Route exact path={Routes.BROWSE} component={Browse} />
+      <Route exact path={Routes.SIGN_IN}>
+        <IsUserRedirect
+          user={user}
+          logPath={Routes.BROWSE}
+          path={Routes.SIGN_IN}
+        >
+          <Signin />
+        </IsUserRedirect>
+      </Route>
+      <Route exact path={Routes.SIGN_UP}>
+        <IsUserRedirect
+          user={user}
+          logPath={Routes.BROWSE}
+          path={Routes.SIGN_UP}
+        >
+          <SignUp />
+        </IsUserRedirect>
+      </Route>
+      <Route exact path={Routes.BROWSE}>
+        <ProtectedRoute user={user} path={Routes.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+      </Route>
       <Route exact path={Routes.HOME} render={() => <Home />} />
       <Route exact render={() => <Redirect to={Routes.HOME} />} />
     </Switch>
--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,1 +1,37 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export function IsUserRedirect({ user, logPath, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+        if (user) {
+          return <Redirect to={{ pathname: logPath }} />;
+        }
+      }}
+    />
+  );
+}
+
+export function ProtectedRoute({ user, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+        if (!user) {
+          console.log("This has run");
+          return (
+            <Redirect to={{ pathname: "sigin", state: { from: location } }} />
+          );
+        }
+      }}
+    />
+  );
+}
